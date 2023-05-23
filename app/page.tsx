dca08537323fe8c453dc26d57e90949f4aef68ab
ryanpearl48 <ryanpearl98@gmail.com>
2023-05-23 10:32:48 -0700

    Started adding styling

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
-export const metadata = {
-  title: 'Inspirational Quote Generator'
-}
+"use client";
+
+import { GradientBackgroundCon } from "@/components/QuoteGenerator/QuoteGeneratorElements"
+
 
 export default function Home() {
   return (
-    <>
-
-    </>
+    <GradientBackgroundCon>
+    </GradientBackgroundCon>
   )
 }
